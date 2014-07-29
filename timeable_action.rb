cb3e30f0c9c99f26c21cdb194152b7964c8cb6b7
Armando Andini <armando.andini@hotmail.com>
2014-07-29 16:01:03 -0300

    Timeable action module.

---
+++ timeable_action.rb
@@ -0,0 +1,36 @@
+module TimeableAction
+
+  module ClassMethods
+    def timeable_action interval, name
+      define_method(name) do |*args, &block|
+        allow_every interval, name do
+          send "#{name}_action", *args
+        end
+      end
+    end
+  end
+
+  def allow_every interval, key, &b
+    if can_perform?(interval, key)
+      reset_timer key
+      yield
+    end
+  end
+
+  def action_timers
+    @timers ||= Hash.new 0
+  end
+
+  def can_perform? interval, key
+    elapsed(key) > interval
+  end
+
+  def elapsed key
+    Time.now.to_f - action_timers[key]
+  end
+
+  def reset_timer key
+    action_timers[key] = Time.now.to_f
+  end
+
+end
