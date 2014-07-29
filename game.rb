8de88d3dbf14f5df3d082e45813053272c0fdc91
Armando Andini <armando.andini@hotmail.com>
2014-07-29 16:01:45 -0300

    Creates the map.

--- game.rb
+++ game.rb
@@ -1,19 +1,21 @@
-require 'observer'
-
 require 'gosu'
-
+require 'pry'
 
 require_relative "snake"
+require_relative "map"
 
 class Game < Gosu::Window
   XRES = 640
   YRES = 640
+
+  attr_accessor :map
 
   def initialize
     super XRES, YRES, false
     @objects = []
     self.caption = "La viborita loca"
     spawn_snake
+    create_map
   end
 
   def update
@@ -36,6 +38,10 @@ class Game < Gosu::Window
   private
     def spawn_snake
       Snake.new self
+    end
+
+    def create_map
+      self.map = Map.new self
     end
 end
 
