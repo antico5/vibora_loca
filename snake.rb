2ac4dffd3183f084aa27827350b4cac671f88b05
Armando Andini <armando.andini@hotmail.com>
2014-07-30 11:40:59 -0300

    Auto moves.

--- snake.rb
+++ snake.rb
@@ -4,35 +4,47 @@ require_relative "timeable_action"
 class Snake
   include Controls
   include TimeableAction
-  extend ClassMethods
 
-  attr_accessor :x, :y, :image, :game
+  attr_accessor :x, :y, :image, :game, :direction
 
   timeable_action 0.5, :move
+
+  DISPLACEMENTS = { down:  [ 0,  1],
+                    up:    [ 0, -1],
+                    right: [ 1,  0],
+                    left:  [-1,  0] }
 
   def initialize game
     game.register self
     @x = @y = 0
     @game = game
     @image = game.image "head.jpg"
+    @direction = :down
   end
 
   def update
     handle_controls
+    auto_move
   end
 
   def draw
     map.draw x, y, image
   end
 
-  def move_action x, y
-    self.x += x
-    self.y += y
+  def move_action direction
+    displacement = DISPLACEMENTS[direction]
+    self.x += displacement[0]
+    self.y += displacement[1]
   end
 
   private
 
+    def auto_move
+      move direction
+    end
+
     def map
       game.map
     end
+
 end
